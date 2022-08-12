3d8b0796066412f2a91071313a604369deef131b
marilozgz <crazygipsypuaj@gmail.com>
2022-08-12 22:20:23 +0200

    jsest

--- src/components/Polls/sum.test.js
+++ src/components/Polls/sum.test.js
@@ -1,11 +1,13 @@
 import { render, screen, cleanup } from "@testing-library/react";
+import jsdom from "jsdom";
 // Importing the jest testing library
 import '@babel/plugin-syntax-jsx'
 import Polls from "./sum";
 import React from "react";
-// afterEach function runs after each test suite is executed
+
+
 describe("<Polls />", () => {
-    test("should display the product page", () => {
+    test("should display the completed questions", () => {
       render(<Polls />);
       expect(
           screen.getByText(/Completed/)
