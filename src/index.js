722b234e566dce1f2fa7e4fdba1cf36a862393c0
marilozgz <crazygipsypuaj@gmail.com>
2022-08-15 16:04:34 +0200

    change style for hovering buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import './index.css';
 
 import "bootstrap/dist/css/bootstrap.css";
 
